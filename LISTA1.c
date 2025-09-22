2d86c59ef4f2d70adc187ae5a0fb105edb515be7
yooshCC <yooshchino@gmail.com>
2025-09-22 20:32:29 +0000

    Estructuras propuestas

--- LISTA1.c
+++ LISTA1.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
-#inlcude <stdlib.h>
+#include <stdlib.h>
+
+//Estructura para el registro de las asistencias de C/u de los Alumnos
+    typedef struct History
+    {
+        int dia, mes, ano;
+        int asistencia_to_day;
+        struct History* sig;
+    }Historial;
+
+//Estrcutura para cada alumno
+    typedef struct Lista_A
+    {
+        char name_Alumno[50];
+        int total_Asistencia;
+        Historial* historial_Academico;
+        struct Lista_A* sig; 
+    }Lista;
+
+//Estructura Principal
+    typedef struct
+    {
+        int no_Alumnos;
+        char name_Curso[50];
+        struct Lista* curso;
+    }Tcurso;
